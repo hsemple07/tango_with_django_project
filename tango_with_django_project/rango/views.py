253096323fe1743dc7d0185ee5f4abedbf7ef8c4
Hugh Semple <3136816S@student.gla.ac.uk>
2026-03-02 18:34:02 +0000

    Chapter 9 completed and tests pass

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from django.urls import reverse
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
 def show_category(request, category_name_slug):
@@ -19,6 +19,7 @@ def show_category(request, category_name_slug):
         
     return render(request, 'rango/category.html', context=context_dict)
 
+@login_required
 def add_category(request):
     form = CategoryForm()
     if request.method == 'POST':
@@ -31,6 +32,7 @@ def add_category(request):
             print(form.errors)
     return(render(request, 'rango/add_category.html', {'form': form}))
 
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -109,8 +111,13 @@ def user_login(request):
             return HttpResponse("Invalid login details supplied.")
     else:
         return render(request, 'rango/login.html')
-    
-    @login_required
-    def restricted(request):
-        return HttpResponse("Since you're logged in, you can see this text!")
+
+@login_required
+def restricted(request):
+    return render(request, 'rango/restricted.html')
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect(reverse('rango:index'))
     
