adc3475ee539aa0b438e1747bc088fceeff8423d
Hugh Semple <3136816S@student.gla.ac.uk>
2026-03-02 21:23:50 +0000

    Chapter 10 Completed

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -68,18 +68,16 @@ def index(request):
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
     visitor_cookie_handler(request)
-    context_dict['visits'] = request.session['visits']
     response = render(request, 'rango/index.html', context=context_dict)
-    visitor_cookie_handler(request)
     return response
 
 def about(request):
     print(request.method)
     print(request.user)
-    if request.session.test_cookie_worked():
-        print("TEST COOKIE WORKED!")
-        request.session.delete_test_cookie()
-    return render(request, 'rango/about.html', {})
+    visitor_cookie_handler(request)
+    context_dict = {}
+    context_dict['visits'] = request.session['visits']
+    return render(request, 'rango/about.html', context=context_dict)
 
 def register(request):
     registered = False
@@ -136,7 +134,7 @@ def get_server_side_cookie(request, cookie, default_val=None):
     return val
     
 def visitor_cookie_handler(request):
-    visits = int(request.COOKIES.get('visits', '1'))
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
     last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
     last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
     if (datetime.now() - last_visit_time).days > 0:
